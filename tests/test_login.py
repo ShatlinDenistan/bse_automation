17613269e327f25f9701b072520973949dcc4e53
Shatlin <shatlin.denistan@takealot.com>
2025-02-20 15:00:51 +0200

    Co-authored-by: Thandeka Tyebileyo <ThandekaMaud@users.noreply.github.com> Co-authored-by: Slindile Mncwango <SlindileTakealot@users.noreply.github.com>

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,5 +1,19 @@
+from playwright.sync_api import Playwright
+
+
 class TestLogin:
-    def test_login_negative_test(self):
-        a = 1
-        b = 2
-        assert a != b
+
+    def test_login_negative_test(self, playwright: Playwright):
+        browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
+        page.goto("https://fin-portal.master.env/login")
+        page.get_by_role("textbox", name="Email").fill("shatlin.denistan@takealot.com")
+        page.get_by_role("textbox", name="Password").fill("testststs")
+        page.get_by_role("button", name="Login").nth(1).click()
+        page.get_by_text("Incorrect username or password").click()
+        page.get_by_role("button", name="Login").click()
+
+        # ---------------------
+        context.close()
+        browser.close()
