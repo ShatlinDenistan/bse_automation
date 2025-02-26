b33836f7aed317f7060134a35492ee355c775488
Shatlin <shatlin.denistan@takealot.com>
2025-02-26 14:21:58 +0200

    added .env file and reading from it

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,4 +1,8 @@
 from playwright.sync_api import Playwright
+import os
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
 
 
 class TestLogin:
@@ -7,13 +11,15 @@ class TestLogin:
         browser = playwright.chromium.launch(headless=False)
         context = browser.new_context()
         page = context.new_page()
-        page.goto("https://fin-portal.master.env/login")
-        page.get_by_role("textbox", name="Email").fill("shatlin.denistan@takealot.com")
-        page.get_by_role("textbox", name="Password").fill("testststs")
+        home_page = os.getenv("HOME_PAGE")
+        page.goto(f"{home_page}/login")
+        user_name = os.getenv("USER_NAME")
+        password = os.getenv("PASSWORD")
+        page.get_by_role("textbox", name="Email").fill(user_name)
+        page.get_by_role("textbox", name="Password").fill(password)
         page.get_by_role("button", name="Login").nth(1).click()
         page.get_by_text("Incorrect username or password").click()
         page.get_by_role("button", name="Login").click()
         # ---------------------
         context.close()
         browser.close()
-
