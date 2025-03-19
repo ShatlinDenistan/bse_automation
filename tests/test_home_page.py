3295179975d7896d10d033ba0c796c4443541ae5
SlindileTakealot <slindile.mncwango@takealot.com>
2025-03-19 13:54:23 +0200

    search by order number

--- tests/test_home_page.py
+++ tests/test_home_page.py
@@ -48,5 +48,32 @@ class TestHomePage:
         context.close()
         browser.close()
 
-    # SLI Order Number
-    # Thandeka RRN
+    @pytest.mark.order_number_search
+    def test_order_number_search(self, playwright: Playwright):
+        """positive login test with valid user name and password"""
+        browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
+        home_page = os.getenv("HOME_PAGE")
+        page.goto(f"{home_page}/login")
+        page.wait_for_load_state('load')
+        user_name = os.getenv("USER_NAME")
+        password = os.getenv("PASSWORD")
+        page.get_by_role("textbox", name="Email").fill(user_name)
+        page.get_by_role("textbox", name="Password").fill(password)
+        page.get_by_role("button", name="Login").nth(1).click()
+        logout_button = page.get_by_role("button", name="Sign Out")
+        expect(logout_button).to_be_visible()
+        search_textbox = page.locator("//input [@name='searchText']")
+        search_filter = page.locator("//div[.='-- all --']").first
+        order_number_option = page.locator("//span[.='Order Number']")
+        search_button = page.locator("//button[@type='submit']")
+        customer_info_button = page.locator("//button[.='Customer Info']")
+        expect(search_textbox).to_be_visible(timeout=5000)
+        search_textbox.fill("10270322")
+        search_filter.click()
+        order_number_option.click()
+        search_button.click()
+        expect(customer_info_button).to_be_visible(timeout=5000)
+        context.close()
+        browser.close()
